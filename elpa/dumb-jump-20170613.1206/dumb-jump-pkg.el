57aafd75de38ba68b7f9e0e36136ea46d9f70591
PengXie <paul_xie@riversecurity.com>
2017-07-03 09:53:40 +0800

    change some packages from site-lisp to elpa.
    install coresspending packages with elpa.
    
    1. names.
    2. s.
    3. dash
    4. dash-at-point

---
+++ elpa/dumb-jump-20170613.1206/dumb-jump-pkg.el
@@ -0,0 +1,1 @@
+(define-package "dumb-jump" "20170613.1206" "jump to definition for multiple languages without configuration." '((emacs "24.3") (f "0.17.3") (s "1.11.0") (dash "2.9.0") (popup "0.5.3")) :commit "aa44169d1d343010e56af0f17bf5448d0dbd2c8f" :keywords '("programming"))
--- lisp/init-dash.el
+++ lisp/init-dash.el
@@ -1,4 +1,5 @@
 (require 'dash-at-point)		; require dash
+(require 'dash)
 (peng-global-set-key (kbd "C-c e") 'dash-at-point-with-docset)
 
 (provide 'init-dash)
