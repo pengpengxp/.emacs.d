1947632a253d108fcda99df93aa4be8df2152835
PengXie <pengpengxppri@sina.com>
2017-09-15 14:40:43 +0800

    Feature: modify bm-toggle-face
    make bm-toggle just underline current line other than highlight it .

--- init.el
+++ init.el
@@ -250,4 +250,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(bm-face ((t (:underline t))) nil "pengpengxp's bm-toggle-face"))
