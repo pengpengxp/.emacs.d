357c5659788a8269415bd7ff1cf7d5075f4c2561
xiepeng <xiepeng@yuqiinf.com>
2017-12-25 11:31:42 +0800

    install bbdb and view-mail

---
+++ lisp/init-vm.el
@@ -0,0 +1,6 @@
+(add-to-list 'load-path
+             (expand-file-name "~/.emacs.d/vm/lisp"))
+(add-to-list 'Info-default-directory-list
+             (expand-file-name "~/.emacs.d/vm/info"))
+(require 'vm-autoloads)
+(provide 'init-vm)
---
+++ vm/vm-load.el
@@ -0,0 +1,6 @@
+; -*- mode: emacs-lisp -*-
+;; Load VM easily
+
+(add-to-list 'load-path "/tmp/vm-8.2.0b-gnu23/vm-8.2.0b/lisp")
+
+(load "vm-autoloads")
