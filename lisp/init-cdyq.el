bd1cca23766f09ffc4b76d72206f004b20fd1cfb
xiepeng <xiepeng@yuqiinf.com>
2018-04-23 15:21:51 +0800

    modify init-cdyq

--- lisp/init-cdyq.el
+++ lisp/init-cdyq.el
@@ -3,7 +3,11 @@
 
   (let* (
          (project-dir "/home/pengpengxp/src/fhos")
-         (arm-env (concat "source " project-dir "/env/fsl_env"))
+         (freescale-string "freescale")
+         (broadcom-string "broadcom")
+         (x86-string "x86")
+         (freescale-env (concat "source " project-dir "/env/fsl_env"))
+         (broadcom-env (concat "source " project-dir "/env/bcm_env"))
          (sai-dir (concat project-dir "/sai"))
          (rpc-dir (concat project-dir "/rpc"))
          (l2-dir (concat project-dir "/l2"))
@@ -14,13 +18,15 @@
          (fhos-common-dir (concat project-dir "/common"))
          (arm-build-dir (concat project-dir "/build/freescale/arm"))
          (x86-build-dir (concat project-dir "/build/freescale/x86"))
-         
+
          (make-command "make clean && bear make -j8 BUILD=debug")
 
          (arch (if (not x)
-                   "arm"
-                 "x86"))
-         
+                   freescale-string
+                 (ivy-read "Which ARCH :" (list freescale-string
+                                                broadcom-string
+                                                x86-string))))
+
          (my-action (ivy-read "Action: " (list
                                           "igmpd"
                                           "l2d"
@@ -33,20 +39,25 @@
                                           )))
 
          (default-directory
-          (cond 
-           ((equal my-action "l2d") l2d-dir)
-           ((equal my-action "igmpd") igmpd-dir)
-           ((equal my-action "l2") l2-dir)
-           ((equal my-action "sai") sai-dir)
-           ((equal my-action "rpc") rpc-dir)
-           ((equal my-action "fhos-common") fhos-common-dir)
-           ((equal my-action "bcm") bcm-dir)
-           ((equal my-action "core") core-dir)
-           ))
+           (cond
+            ((equal my-action "l2d") l2d-dir)
+            ((equal my-action "igmpd") igmpd-dir)
+            ((equal my-action "l2") l2-dir)
+            ((equal my-action "sai") sai-dir)
+            ((equal my-action "rpc") rpc-dir)
+            ((equal my-action "fhos-common") fhos-common-dir)
+            ((equal my-action "bcm") bcm-dir)
+            ((equal my-action "core") core-dir)
+            ))
          )
-    (let ((my-command (if (equal "arm" arch)
-                          (concat arm-env " && " make-command)
-                        make-command)))
+    (let ((my-command (cond
+                       ((equal freescale-string arch)
+                        (concat freescale-env " && " make-command))
+
+                       ((equal broadcom-string arch)
+                        (concat broadcom-env " && " make-command))
+
+                       (t make-command))))
       (progn
         (compile my-command)
         (switch-to-buffer-other-window "*compilation*")))))
